ce490e0d7099eebee2b808172d45250603b01985
vis-19maya <165902604+vis-19maya@users.noreply.github.com>
2024-04-05 17:02:42 +0530

    Add files via upload

---
+++ src/pages/forget.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Forget from '../components/forget';
+
+const ForgetPage= () => {
+
+  return (
+    <Forget />
+  );
+};
+
+export default ForgetPage;
---
+++ src/pages/registerPage.jsx
@@ -0,0 +1,9 @@
+import Register from '../components/register';
+
+function RegisterPage() {
+  return (
+   <Register />
+  );
+}
+
+export default RegisterPage;
